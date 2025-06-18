a72cf44d5399d4e77ca5612984c3c8496396321e
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-06-18 21:47:22 +0300

    Update unittest_main.py

--- python/unittest_main.py
+++ python/unittest_main.py
@@ -33,23 +33,11 @@ class TestArrayFunctions(unittest.TestCase):
         self.assertEqual(len(divided_subarrays), desired_number_of_subarrays)
         self.assertTrue(all(len(subarray) <= 1 for subarray in divided_subarrays))
 
-    def test_divide_array_into_subarrays_zero_parts(self):
-        original_input_array = [1, 2, 3]
-        desired_number_of_subarrays = 0
-        with self.assertRaises(ValueError):
-            divide_array_into_subarrays(original_input_array, desired_number_of_subarrays)
-
     def test_find_max_in_subarray(self):
         input_subarray = [1, 3, 2, 5, 4]
         max_results_storage = [0] * 1  # Массив для хранения результата
         find_max_in_subarray(input_subarray, max_results_storage, 0)
         self.assertEqual(max_results_storage[0], max(input_subarray))
-
-    def test_find_max_in_empty_subarray(self):
-        empty_input_subarray = []
-        max_results_storage = [0] * 1
-        with self.assertRaises(IndexError):
-            find_max_in_subarray(empty_input_subarray, max_results_storage, 0)
 
     def test_find_max_in_single_element_subarray(self):
         single_element_subarray = [42]
