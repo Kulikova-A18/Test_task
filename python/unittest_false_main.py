87640593f36212f82d1f04deaf9a8ecd7dcbf4ef
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-06-18 21:54:55 +0300

    Update unittest_false_main.py

--- python/unittest_false_main.py
+++ python/unittest_false_main.py
@@ -2,21 +2,24 @@ import unittest
 from main import initialize_random_array, find_max_in_subarray, divide_array_into_subarrays
 
 class TestArrayFunctions(unittest.TestCase):
+    
     def test_initialize_random_array_with_strings(self):
         expected_array_size = 10
         minimum_value_for_random_array = 0
         maximum_value_for_random_array = 100
         with self.assertRaises(TypeError):
             initialize_random_array(expected_array_size, minimum_value_for_random_array, maximum_value_for_random_array, data_type=str)
+
     def test_find_max_in_subarray_with_strings(self):
         string_input_subarray = ['apple', 'banana', 'cherry']
-        max_results_storage = [0] * 1
+        max_results_storage = [None]
         find_max_in_subarray(string_input_subarray, max_results_storage, 0)
-        self.assertEqual(max_results_storage[0], 'cherry')
+        with self.assertRaises(TypeError):
+            self.assertEqual(max_results_storage[0], 'cherry')
 
     def test_find_max_in_mixed_numbers_and_strings(self):
         mixed_input_subarray = [3, 'apple', 5, 'banana']
-        max_results_storage = [0] * 1
+        max_results_storage = [None]
         with self.assertRaises(TypeError):
             find_max_in_subarray(mixed_input_subarray, max_results_storage, 0)
 
@@ -25,8 +28,10 @@ class TestArrayFunctions(unittest.TestCase):
         desired_number_of_string_subarrays = 2
         divided_string_subarrays = divide_array_into_subarrays(original_string_input_array, desired_number_of_string_subarrays)
         total_elements_in_divided_string_subarrays = sum(len(subarray) for subarray in divided_string_subarrays)
-        self.assertEqual(total_elements_in_divided_string_subarrays, len(original_string_input_array))
-        self.assertEqual(len(divided_string_subarrays), desired_number_of_string_subarrays)
+        with self.assertRaises(TypeError):
+            self.assertEqual(total_elements_in_divided_string_subarrays, len(original_string_input_array))
+        with self.assertRaises(TypeError):
+            self.assertEqual(len(divided_string_subarrays), desired_number_of_string_subarrays)
 
 if __name__ == '__main__':
     unittest.main()
