e4c7df89d899b9872247982d07bfc273b5729424
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-06-18 21:57:09 +0300

    Update unittest_false_main.py

--- python/unittest_false_main.py
+++ python/unittest_false_main.py
@@ -12,14 +12,13 @@ class TestArrayFunctions(unittest.TestCase):
 
     def test_find_max_in_subarray_with_strings(self):
         string_input_subarray = ['apple', 'banana', 'cherry']
-        max_results_storage = [None]
+        max_results_storage = [None]  # Используем None вместо 0 для хранения результата
         find_max_in_subarray(string_input_subarray, max_results_storage, 0)
-        with self.assertRaises(TypeError):
-            self.assertEqual(max_results_storage[0], 'cherry')
+        self.assertEqual(max_results_storage[0], 'cherry')
 
     def test_find_max_in_mixed_numbers_and_strings(self):
         mixed_input_subarray = [3, 'apple', 5, 'banana']
-        max_results_storage = [None]
+        max_results_storage = [None]  # Используем None вместо 0 для хранения результата
         with self.assertRaises(TypeError):
             find_max_in_subarray(mixed_input_subarray, max_results_storage, 0)
 
@@ -28,10 +27,8 @@ class TestArrayFunctions(unittest.TestCase):
         desired_number_of_string_subarrays = 2
         divided_string_subarrays = divide_array_into_subarrays(original_string_input_array, desired_number_of_string_subarrays)
         total_elements_in_divided_string_subarrays = sum(len(subarray) for subarray in divided_string_subarrays)
-        with self.assertRaises(TypeError):
-            self.assertEqual(total_elements_in_divided_string_subarrays, len(original_string_input_array))
-        with self.assertRaises(TypeError):
-            self.assertEqual(len(divided_string_subarrays), desired_number_of_string_subarrays)
+        self.assertEqual(total_elements_in_divided_string_subarrays, len(original_string_input_array))
+        self.assertEqual(len(divided_string_subarrays), desired_number_of_string_subarrays)
 
 if __name__ == '__main__':
     unittest.main()
